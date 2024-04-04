0f84ab16d0a1b78941ffc7c4dde1f2f20d70f90e
Sharmelyn <extsbautista@utp.edu.pe>
2024-04-04 07:13:11 -0500

    fix/quitando lo que no se usa

--- help/admin.py
+++ help/admin.py
@@ -3,21 +3,11 @@ from help.forms import RequestAttentionForm
 from help.models import Help, RequestAttention
 from django.db.models import Q
 
-def marcar_como_en_proceso(queryset):
-    queryset.update(estado='en proceso')
-
-def marcar_como_finalizado(queryset):
-    queryset.update(estado='finalizado')
-
-marcar_como_en_proceso.short_description = "Marcar como En proceso"
-marcar_como_finalizado.short_description = "Marcar como Finalizado"
-
 @admin.register(RequestAttention)
 class RequestAttentionAdmin(admin.ModelAdmin):
     form = RequestAttentionForm
     list_display = ('date_of_attention', 'time_of_attention', 'PAMid', 'user', 'type', 'telefono_de_contacto', 'estado', 'atendido_por', 'details')
     search_fields = ('user__username', 'PAMid')
-    actions = [marcar_como_en_proceso, marcar_como_finalizado]
     def get_form(self, request, obj=None, **kwargs):
         form = super().get_form(request, obj, **kwargs)
         form.base_fields['estado'].widget.can_add_related = False 
